02895eb29dbb4e2fac1eb81fe58189571db53058
Jérôme CHEVIET <jcheviet@hotmail.com>
2021-09-07 22:04:42 +0200

    Add JavaDoc comments in AnalyticsCounter
      * Clean variable i line 17
      * Clean iteration i++ line 20
      * Add class's javadoc
      * Add main's javadoc
      * Add TODO for futur improvement

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,21 +4,33 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 
+/**
+ * Class to count number of symptom in file symptoms.txt
+ *
+ * @author J.Cheviet
+ * @version 1.0.0
+ *
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
+
+	/**
+	 * Main class
+	 *
+	 * Read all line of file symptoms.txt, count the number of headache, rash and deleted pupils.
+	 * The total is register in file result.out
+	 */
 	public static void main(String args[]) throws Exception {
-		// first get input
 		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
 		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		//int headCount = 0;	// counts headaches
+		//TODO Change while to method
 		while (line != null) {
-			i++;	// increment i
 			System.out.println("symptom from file: " + line);
+			
+			//TODO study array to recording all symptom in file
 			if (line.equals("headache")) {
 				headacheCount++;
 				System.out.println("number of headaches: " + headacheCount);
@@ -33,7 +45,7 @@ public class AnalyticsCounter {
 			line = reader.readLine();	// get another symptom
 		}
 		
-		// next generate output
+		//TODO transform to method
 		FileWriter writer = new FileWriter ("result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
