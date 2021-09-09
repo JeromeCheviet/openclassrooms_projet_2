7c3d321a95c8109193280d794b61522dcb72fafe
Jerome Cheviet <admin@gharan.ovh>
2021-09-09 13:37:44 +0200

    write number of symptoms in alphabetical order
      * Create a dictionnary  with "HashMap" call symptoms to add all
    symptoms and number of occurences into.
      * Transform this dictionnary with "TreeMaps". Now all symptoms can be
    sort in alphabetical order.
      * With loop "for" the symptoms with the number off occurences for each
    are writing in alphabetical order in file "result.out"

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,53 +3,52 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * Class to count number of symptom in file symptoms.txt
  *
  * @author J.Cheviet
  * @version 1.0.0
- *
  */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
 
-	/**
-	 * Main class
-	 *
-	 * Read all line of file symptoms.txt, count the number of headache, rash and deleted pupils.
-	 * The total is register in file result.out
-	 */
-	public static void main(String args[]) throws Exception {
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+    /**
+     * Main class
+     * <p>
+     * Read all line of file symptoms.txt, count the number of headache, rash and deleted pupils.
+     * The total is register in file "result.out"
+     */
+    public static void main(String[] args) throws Exception {
+        //TODO create variable with path file.
+        BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+        String line = reader.readLine();
 
-		//TODO Change while to method
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			
-			//TODO study array to recording all symptom in file
-			if (line.equals("headache")) {
-				headacheCount++;
-				System.out.println("number of headaches: " + headacheCount);
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
+        Map<String, Integer> symptoms = new HashMap<>();
 
-			line = reader.readLine();	// get another symptom
-		}
-		
-		//TODO transform to method
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
-	}
+        //TODO Change while to method
+        while (line != null) {
+            if (symptoms.containsKey(line)) {
+                int newCount = symptoms.get(line) + 1;
+                symptoms.put(line, newCount);
+            } else {
+                symptoms.put(line, 1);
+            }
+
+            line = reader.readLine();
+        }
+        reader.close();
+
+        //TODO transform to method
+        Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
+
+        FileWriter writer = new FileWriter ("result.out");
+
+        for (Map.Entry<String,Integer> symptom : sortedSymptoms.entrySet()) {
+            writer.write(symptom.getKey() + " : " + symptom.getValue() + "\n");
+        }
+        writer.close();
+    }
 }
