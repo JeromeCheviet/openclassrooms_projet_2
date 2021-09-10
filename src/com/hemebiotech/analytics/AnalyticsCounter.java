26a45b55c07c2ad9560e5f297618623be6ecdb39
Jerome Cheviet <admin@gharan.ovh>
2021-09-10 14:33:44 +0200

    Change HashMap's class by TreeMap's class
    To optimized the application, the dictionary use to put and count each
    symptom is directly create with TreeMap and directly right in
    alphabetical order in the file "result.out".

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,30 +3,30 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
 /**
- * Class to count number of symptom in file symptoms.txt
+ * <g>Class to count number of symptom in file symptoms.txt</g>
  *
  * @author J.Cheviet
- * @version 1.0.0
+ * @version 1.0.1
  */
 public class AnalyticsCounter {
 
     /**
-     * Main class
-     * <p>
-     * Read all line of file symptoms.txt, count the number of headache, rash and deleted pupils.
-     * The total is register in file "result.out"
+     * <u>Main class</u>
+     *
+     * <p>Read the file "symptoms.txt" and put each line into a dictionary <i>TreeMap</i>'s class and implement the
+     * number of symptom in alphabetical order.
+     * The data are register in the file "result.out"</p>
      */
     public static void main(String[] args) throws Exception {
         //TODO create variable with path file.
         BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
         String line = reader.readLine();
 
-        Map<String, Integer> symptoms = new HashMap<>();
+        Map<String, Integer> symptoms = new TreeMap<>();
 
         //TODO Change while to method
         while (line != null) {
@@ -42,11 +42,9 @@ public class AnalyticsCounter {
         reader.close();
 
         //TODO transform to method
-        Map<String, Integer> sortedSymptoms = new TreeMap<>(symptoms);
+        FileWriter writer = new FileWriter("result.out");
 
-        FileWriter writer = new FileWriter ("result.out");
-
-        for (Map.Entry<String,Integer> symptom : sortedSymptoms.entrySet()) {
+        for (Map.Entry<String, Integer> symptom : symptoms.entrySet()) {
             writer.write(symptom.getKey() + " : " + symptom.getValue() + "\n");
         }
         writer.close();
